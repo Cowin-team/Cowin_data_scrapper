b27aca2d2607725b1788e11f6d5f05721b1827f1
Vipin Verma <vipin8169@gmail.com>
2021-05-04 02:20:00 -0700

    Show total beds in denominator

--- src/covidpune.js
+++ src/covidpune.js
@@ -1,22 +1,23 @@
 // this script parses the data from covidpune.com
+var outputJsonArray = [];
 fetch("https://covidpune.com/data/covidpune.com/bed_data.json")
   .then(function (response) {
     return response.json();
   })
   .then(function (bedJson) {
-    var outputJsonArray = [];
+
     for (var key in bedJson) {
       var rowJson = {};
       var rowdata = bedJson[key];
       rowJson["Name"] = rowdata["hospital_name"];
-      rowJson["COVID_beds"] = rowdata["available_beds_without_oxygen"];
-      rowJson["Oxygen_beds"] = rowdata["available_beds_with_oxygen"];
-      rowJson["ICU"] = rowdata["available_icu_beds_without_ventilator"];
-      rowJson["Ventilator_Beds"] = rowdata["available_icu_beds_with_ventilator"];
+      rowJson["COVID_beds"] = rowdata["available_beds_without_oxygen"] + "/" + rowdata["total_beds_without_oxygen"];
+      rowJson["Oxygen_beds"] = rowdata["available_beds_with_oxygen"] + "/" + rowdata["total_beds_with_oxygen"];
+      rowJson["ICU"] = rowdata["available_icu_beds_without_ventilator"] + "/" + rowdata["total_icu_beds_without_ventilator"];
+      rowJson["Ventilator_Beds"] = rowdata["available_icu_beds_with_ventilator"] + "/" + rowdata["total_icu_beds_with_ventilator"];
       rowJson["Contact"] = rowdata["hospital_phone"];
       rowJson["Area"] = rowdata["area"];
       rowJson["Hospital_category"] = rowdata["hospital_category"];
-      rowJson["Last_updated"] = new Date(rowdata["last_updated_on"]).toLocaleString(undefined, {timeZone: 'Asia/Kolkata'});;
+      rowJson["Last_updated"] = new Date(rowdata["last_updated_on"]).toLocaleString(undefined, {timeZone: 'Asia/Kolkata'});
       outputJsonArray.push(rowJson);
     }
     console.log("outputJsonArray == " + JSON.stringify(outputJsonArray));
