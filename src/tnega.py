5b4135a6bb32008c577e33e47a425af2c05e1f97
Krishnan Gopalakrishnan <krishnan_gopalakrishnan@teamhealth.com>
2021-05-06 20:08:33 -0400

    updated to fetch more data

--- src/tnega.py
+++ src/tnega.py
@@ -5,36 +5,41 @@ from datetime import datetime
 import os
 from tnega_districtmapping import sheet_district_map
 
-# define os separator:
-sep = os.path.sep
+if __name__ == '__main__':
+  try:
 
-# get src dir:
-srcdir = os.path.dirname(os.path.abspath(__file__))
+    # define os separator:
+    sep = os.path.sep
 
-# initialize output list:
-APIinput = []
+    # get src dir:
+    srcdir = os.path.dirname(os.path.abspath(__file__))
 
-# iterate the district map:
-for sheetName in sheet_district_map:
-  fetch_data = {
-                "District": f"{sheetName}",
-                "FacilityTypes": ["CHO", "CHC", "CCC"],
-                "IsGovernmentHospital": True,
-                "IsPrivateHospital": True,
-                "pageLimit": 1
-              }
-  res = requests.post('https://tncovidbeds.tnega.org/api/hospitals', data = json.dumps(fetch_data), headers={'Content-Type': 'application/json', 'Accept': 'text/plain'})
-  res_json = res.json()
-  for rec in res_json['result']:
-    stack = {}
-    stack['Sheet Name']=f"{sheetName}"
-    stack['Name']=rec.get('Name','N/A')
-    stack['URL']=""
-    stack['COVID Beds']=rec['CovidBedDetails'].get('VaccantNonO2Beds','N/A')
-    stack['Oxygen Beds']=rec['CovidBedDetails'].get('VaccantO2Beds','N/A')
-    stack['ICU']=rec['CovidBedDetails'].get('VaccantICUBeds','N/A')
-    stack['LAST UPDATED']=datetime.fromtimestamp(rec['CovidBedDetails'].get('UpdatedOn', 'N/A')).strftime('%Y-%m-%d %H:%M:%S')
-    APIinput.append(stack)
+    # initialize output list:
+    APIinput = []
 
-# write output to JSON file:
-json.dump(APIinput, open(f'{srcdir}{sep}output{sep}APIinput_tnega.json', mode='w'), indent=2)
+    # iterate the district map:
+    for sheetName in sheet_district_map:
+      fetch_data = {
+                    "District": f"{sheetName}",
+                    "FacilityTypes": ["CHO", "CHC", "CCC"],
+                    "IsGovernmentHospital": True,
+                    "IsPrivateHospital": True,
+                    "pageLimit": 100
+                  }
+      res = requests.post('https://tncovidbeds.tnega.org/api/hospitals', data = json.dumps(fetch_data), headers={'Content-Type': 'application/json', 'Accept': 'text/plain'})
+      res_json = res.json()
+      for rec in res_json['result']:
+        stack = {}
+        stack['Sheet Name']=f"{sheetName}"
+        stack['Name']=rec.get('Name','N/A')
+        stack['URL']=""
+        stack['COVID Beds']=rec['CovidBedDetails'].get('VaccantNonO2Beds','N/A')
+        stack['Oxygen Beds']=rec['CovidBedDetails'].get('VaccantO2Beds','N/A')
+        stack['ICU']=rec['CovidBedDetails'].get('VaccantICUBeds','N/A')
+        stack['LAST UPDATED']=datetime.fromtimestamp(rec['CovidBedDetails'].get('UpdatedOn', 'N/A')).strftime('%Y-%m-%d %H:%M:%S')
+        APIinput.append(stack)
+
+    # write output to JSON file:
+    json.dump(APIinput, open(f'{srcdir}{sep}output{sep}APIinput_tnega.json', mode='w'), indent=2)
+  except Exception as err:
+    print(err)
