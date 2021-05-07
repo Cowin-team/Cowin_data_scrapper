46a548ad5597dfa60bd8c7587350a7139821c5f1
Krishnan Gopalakrishnan <krishnan_gopalakrishnan@teamhealth.com>
2021-05-07 19:28:03 -0400

    updated the api call logic

--- src/tnega.py
+++ src/tnega.py
@@ -1,9 +1,12 @@
-
+#!/usr/bin/python3
 import requests
 import json
 from datetime import datetime
 import os
 from tnega_districtmapping import sheet_district_map
+
+class APICallFailure(Exception):
+  pass
 
 if __name__ == '__main__':
   try:
@@ -16,6 +19,9 @@ if __name__ == '__main__':
 
     # initialize output list:
     APIinput = []
+
+    # API url:
+    api_url = 'http://127.0.0.1:5000/update'
 
     # iterate the district map:
     for sheetName, districtKey in sheet_district_map.items():
@@ -30,16 +36,23 @@ if __name__ == '__main__':
       res_json = res.json()
       for rec in res_json['result']:
         stack = {}
-        stack['Sheet Name']=' '.join([rec.get('District').get('Name'),'Beds'])
+        stack['Sheet Name']=' '.join([rec.get('District','N/A').get('Name','N/A'),'Beds'])
         stack['Name']=rec.get('Name','N/A')
         stack['URL']=""
-        stack['COVID Beds']=rec['CovidBedDetails'].get('VaccantNonO2Beds','N/A')
-        stack['Oxygen Beds']=rec['CovidBedDetails'].get('VaccantO2Beds','N/A')
+        stack['COVID Beds']=rec.get('CovidBedDetails', 'N/A').get('VaccantNonO2Beds','N/A')
+        stack['Oxygen Beds']=rec.get('CovidBedDetails','N/A').get('VaccantO2Beds','N/A')
         stack['ICU']=rec['CovidBedDetails'].get('VaccantICUBeds','N/A')
-        stack['LAST UPDATED']=datetime.fromtimestamp(rec['CovidBedDetails'].get('UpdatedOn', 'N/A')).strftime('%Y-%m-%d %H:%M:%S')
+        stack['LAST UPDATED']=datetime.fromtimestamp(rec.get('CovidBedDetails','N/A').get('UpdatedOn', 'N/A')).strftime('%Y-%m-%d %H:%M:%S')
         APIinput.append(stack)
 
     # write output to JSON file:
-    json.dump(APIinput, open(f'{srcdir}{sep}output{sep}APIinput_tnega.json', mode='w'), indent=2)
+    #json.dump(APIinput, open(f'{srcdir}{sep}output{sep}APIinput_tnega.json', mode='w'), indent=2)
+    # invoke the api to update sheet:
+    for _json in APIinput:
+      api_response = requests.post(api_url, json=json.loads(_json), verify=False)
+      if api_response.status_code != 200:
+        raise APICallFailure(api_response.text)
+  except APICallFailure as err:
+    print(err)
   except Exception as err:
     print(err)
