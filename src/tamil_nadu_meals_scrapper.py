c55fe8b57ed6876d32aa3b522af6d529db57cdbc
Vivek-95 <mvvivek@hotmail.com>
2021-05-10 23:41:00 -0700

    Added remove duplicates for subset columns in treat duplicates function

--- src/tamil_nadu_meals_scrapper.py
+++ src/tamil_nadu_meals_scrapper.py
@@ -8,7 +8,7 @@ d = sheets.get(sheet_url)
 murshitha = d.sheets[3].to_frame(usecols=range(14))
 ## Harshini  meal services.
 harshini = d.sheets[4].to_frame()
-## Food delivery Old meal services -- Do not use. Old version
+## Food delivery Old meal services
 # df = d.sheets[5].to_frame()
 
 def get_clean_contact_info(df,col_name):
@@ -27,7 +27,6 @@ def get_clean_contact_info(df,col_name):
     return df
 
 ## Drop rows where contact info is null
-cleaned_df = get_clean_contact_info(df,'Contact')
 murshitha = get_clean_contact_info(murshitha,'Mobile \nNumber')
 harshini = get_clean_contact_info(harshini,'Contact Number')
 
@@ -46,6 +45,18 @@ def concatenate_column_values_to_comments_section(df,col_names,fill_na='No Value
     return df
 
 def treat_duplicates(df,ph_no_col_name,check_unique_col_names):
+    duplicated_phone_numbers = df[ph_no_col_name][df[ph_no_col_name].duplicated()]
+    for number in duplicated_phone_numbers:
+        temp = df[df['Phone Number'] == number].fillna('No Value Present')
+        rows = {}
+        for col in check_unique_col_names:
+            unique = ','.join(temp[col].str.strip().unique())
+#             df.loc[(df['Phone Number'] == number).index][col] = unique
+            temp[col] = unique
+        df[df['Phone Number'] == number] = temp
+    return df.drop_duplicates(subset=check_unique_col_names)
+
+def treat_duplicates(df,ph_no_col_name,check_unique_col_names):
     """
     Function to treat duplicates.
     :param df: Dataframe
@@ -56,13 +67,11 @@ def treat_duplicates(df,ph_no_col_name,check_unique_col_names):
     duplicated_phone_numbers = df[ph_no_col_name][df[ph_no_col_name].duplicated()]
     for number in duplicated_phone_numbers:
         temp = df[df['Phone Number'] == number].fillna('No Value Present')
-        rows = {}
         for col in check_unique_col_names:
             unique = ','.join(temp[col].str.strip().unique())
-#             df.loc[(df['Phone Number'] == number).index][col] = unique
             temp[col] = unique
         df[df['Phone Number'] == number] = temp
-    return df.drop_duplicates()
+    return df.drop_duplicates(subset=check_unique_col_names)
 
 
 ## Standardize to meals format
@@ -71,7 +80,7 @@ target_columns = ['Name','Address','Latitude','Longitude','Phone Number','Type',
 #################### Harshini - Start
 ## Rename columns in Harshini to columns in target columns
 harshini = harshini.rename({'Prepared By':'Name','Contact Number':'Phone Number'},axis=1)
-harshini=treat_duplicates(harshini,'Phone Number',['Area', 'Order Placement', 'Delivery Option',
+harshini=treat_duplicates(harshini,'Phone Number',['Phone Number','Area', 'Order Placement', 'Delivery Option',
        'Delivery Time', 'Delivered by', 'Notes', 'Max order that can be placed'])
 harshini = concatenate_column_values_to_comments_section(harshini,['Area', 'Order Placement', 'Delivery Option',
        'Delivery Time', 'Delivered by', 'Notes', 'Max order that can be placed'])
@@ -84,7 +93,7 @@ murshitha['Name'] += '\nContact Name: ' + murshitha['Contact Name'].fillna('No V
 
 ## Rename columns in Harshini to columns in target columns
 murshitha = murshitha.rename({'Meal Type':'Type','Mobile \nNumber':'Phone Number'},axis=1)
-murshitha = treat_duplicates(murshitha,'Phone Number',['Location of Kitchen', 'Delivery Mode', 'Any other details you would like to share?',
+murshitha = treat_duplicates(murshitha,'Phone Number',['Phone Number','Location of Kitchen', 'Delivery Mode', 'Any other details you would like to share?',
        'Do you offer Covid Specific Diet food?', 'Would you consider donating some meals to those who are isolated but unable to afford to order meals?'
         ,'Verified'])
 murshitha = concatenate_column_values_to_comments_section(murshitha,['Location of Kitchen', 'Delivery Mode', 'Any other details you would like to share?',
@@ -95,8 +104,9 @@ murshitha = murshitha.reindex(columns=target_columns)
 #################### murshitha - Complete
 
 
+
 meals_df = pd.concat([harshini,murshitha])
-final_meals_df = treat_duplicates(meals_df,'Phone Number',[ 'Type','Comments', 'Cost Type']).fillna('No Value Present')
+final_meals_df = treat_duplicates(meals_df,'Phone Number',[ 'Phone Number','Type','Comments']).fillna('No Value Present')
 
 ## Store dataframe
-final_meals_df.to_csv('tamil_nadu_meals.csv')
+final_meals_df.to_csv('tamil_nadu_meals.csv',index=False)
