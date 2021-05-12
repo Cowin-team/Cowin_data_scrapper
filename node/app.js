53252543a0366d43e02b84d4bff8c5ee3b5a55b3
Rakshith <rakshith.2905@gmail.com>
2021-05-11 23:02:58 -0700

    added tamilnadu

--- node/app.js
+++ node/app.js
@@ -1,5 +1,5 @@
-// var express = require('express');
-// // var app = express();
+var express = require('express');
+var app = express();
 
 const pune = require('./services/pune');
 const tamilnadu = require('./services/tamilnadu');
@@ -9,12 +9,38 @@ const telangana = require('./services/telangana');
 const sheetsAPI = require('./services/sheetsAPI');
 
 
-telangana.fetchData().then((response) => {
-    // console.log(response)
-    sheetsAPI.send(response).then((res) => {
-        console.log(res)
+app.get('/pune', function (req, res) {
+    pune.fetchData().then((response) => {
+        sheetsAPI.send(response).then((res) => {
+            res.send(res)
+        })
     })
-})
+ })
+
+app.get('/telangana', function (req, res) {
+    telangana.fetchData().then((response) => {
+        sheetsAPI.send(response).then((res) => {
+            res.send(res)
+        })
+    })
+ })
+
+ app.get('/mumbai', function (req, res) {
+    mumbai.fetchData().then((response) => {
+        sheetsAPI.send(response).then((res) => {
+            res.send(res)
+        })      
+    })
+ })
+
+ app.get('/tamilnadu', function (req, res) {
+    tamilnadu.fetchData().then((response) => {
+        res.send("Updating TamilNadu")
+        sheetsAPI.send(response).then((res) => {
+            res.send(res)
+        })
+    })
+ })
 
 // app.get('/mumbai', function (req, res) {
 //     mumbai.fetchData().then((response) => {
@@ -22,10 +48,10 @@ telangana.fetchData().then((response) => {
 //     })
 //     // res.send('Hello World');
 //  })
- 
-//  var server = app.listen(8081, function () {
-//     var host = server.address().address
-//     var port = server.address().port
+
+ var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
     
-//     console.log("Example app listening at http://%s:%s", host, port)
-//  })
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
