a08a604da27621a18e95baf57d423e94875bee2d
Krishnan Gopalakrishnan <krishnan_gopalakrishnan@teamhealth.com>
2021-05-22 20:48:39 -0400

    parsed and update contacts for each hospital

--- src/nashik.py
+++ src/nashik.py
@@ -1,56 +1,73 @@
 # %%
-#!/usr/bin/python3
+# !/usr/bin/python3
 import pandas as pd
 import json
 from collections import OrderedDict
 import requests
+from bs4 import BeautifulSoup as bs
+import re
+
+# regex compile:
+_CLEAN_PATTERN = re.compile(r"(\s|\n|\-)")
 
 # %%
 if __name__ == '__main__':
-  # define the website url to be scraped:
-  site_url = 'http://covidcbrs.nmc.gov.in/home/searchHosptial'
+    # define the website url to be scraped:
+    site_url = 'http://covidcbrs.nmc.gov.in/home/searchHosptial'
 
-  # read site html table using pandas:
-  df = pd.read_html(site_url)
+    # read site html table using pandas:
+    df = pd.read_html(site_url)
 
-  # initialize result dict:
-  result = OrderedDict()
+    # get contacts from the URL response
+    # this piece can be optimized to get all the information
+    # from single URL post but due to lack of time adding
+    # separate calls to get clean Hospital contact number
+    res = requests.post(site_url, data={"bed_aval": "all", "hosptype": "all", "search": "Search"})
+    soup = bs(res.content, 'html.parser')
+    _get_rows = [s.text for s in soup.find_all('tr', recursive=True)]
+    _hosp_contacts = []
+    # skipping header and footer of the tr tags:
+    for i, row in enumerate(_get_rows[2:-2], 1):
+      _hosp_contacts.append(_CLEAN_PATTERN.sub('', [r.split(':')[1] if len(r.split(':')) > 1 else r for r in row.split('\n\n')][12]))
 
-  # define the update url:
-  update_api_url = 'http://127.0.0.1:5000/updateBulk'
+    # initialize result dict:
+    result = OrderedDict()
 
-  # iterate rows from output list
-  # skipping last two rows as it contains cumulative total:
-  for row in df:
-    result['Name'] = row.get('Hospital Name').get(
-          'Hospital Name').tolist()[:-2]
-    result['COVID Beds'] = row.get(
-          'General Beds').get('Vacant').tolist()[:-2]
-    result['Oxygen Beds'] = row.get(
-          'Oxygen Beds').get('Vacant').tolist()[:-2]
-    result['ICU'] = row.get('ICU Beds').get('Vacant').tolist()[:-2]
-    result['Ventilator'] = row.get(
-          'Ventilator Beds').get('Vacant').tolist()[:-2]
-    result['LAST UPDATED'] = ['N/A']*int(len(row) - 2)
-    result['Contact'] = ['N/A']*int(len(row) - 2)
-    result['Check LAST UPDATED'] = ['False']*int(len(row) - 2)
-    result['Sheet Name'] = ['Nashik Beds']*int(len(row) - 2)
+    # define the update url:
+    update_api_url = 'http://127.0.0.1:5000/updateBulk'
 
-  # create normalized dataframe from result dict:
-  norm_df = pd.DataFrame.from_dict(result)
+    # iterate rows from output list
+    # skipping last two rows as it contains cumulative total:
+    for row, _contact in zip(df, [_hosp_contacts]):
+        result['Name'] = row.get('Hospital Name').get(
+            'Hospital Name').tolist()[:-2]
+        result['COVID Beds'] = row.get(
+            'General Beds').get('Vacant').tolist()[:-2]
+        result['Oxygen Beds'] = row.get(
+            'Oxygen Beds').get('Vacant').tolist()[:-2]
+        result['ICU'] = row.get('ICU Beds').get('Vacant').tolist()[:-2]
+        result['Ventilator'] = row.get(
+            'Ventilator Beds').get('Vacant').tolist()[:-2]
+        result['LAST UPDATED'] = ['N/A']*int(len(row) - 2)
+        result['Contact'] = _contact
+        result['Check LAST UPDATED'] = ['False']*int(len(row) - 2)
+        result['Sheet Name'] = ['Nashik Beds']*int(len(row) - 2)
 
-  # clean hospital name:
-  norm_df['Name'] = norm_df['Name'].apply(
-      lambda x: x.replace("(Click here for contact details)", ""))
+    # create normalized dataframe from result dict:
+    norm_df = pd.DataFrame.from_dict(result)
 
-  # update sheets using the api call:
-  try:
-    APIInput = norm_df.to_json(orient='records', indent=2)
-    print(APIInput)
-    api_response = requests.post(
-          update_api_url, json=json.loads(APIInput), verify=False)
-    print(api_response.text)
-    if api_response.status_code != 200:
-      raise Exception(f"bulkupdate failed: {api_response.text}")
-  except Exception as err:
-    print(err)
+    # clean hospital name:
+    norm_df['Name'] = norm_df['Name'].apply(
+        lambda x: x.replace("(Click here for contact details)", ""))
+
+    # update sheets using the api call:
+    try:
+        APIInput = norm_df.to_json(orient='records', indent=2)
+        print(APIInput)
+        api_response = requests.post(
+            update_api_url, json=json.loads(APIInput), verify=False)
+        print(api_response.text)
+        if api_response.status_code != 200:
+            raise Exception(f"bulkupdate failed: {api_response.text}")
+    except Exception as err:
+        print(err)
