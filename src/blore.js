569869a338ea03eb05be143eb28922f645c79caa
Aneesha Punreddy <aneeshapunreddy@gmail.com>
2021-05-31 13:33:25 -0700

    Bangalore Script with PowerBI - first working version which extracts 100 hospitals

--- src/blore.js
+++ src/blore.js
@@ -1,57 +1,110 @@
-// this script parses the data from http://covid.rcmedicrew.org/ using their /scripts/getSearch.php API
-// install nodejs then npm install axios
 const axios = require('axios');
 const cheerio = require('cheerio');
-const fetch = require("node-fetch");
-const moment = require('moment');
 const puppeteer = require('puppeteer');
-const qs = require('qs')
-// const converter = require('json-2-csv');
+const fetch = require("node-fetch");
 
-const siteURL = "http://apps.bbmpgov.in/covid19/index.html";
+const siteURL = "http://apps.bbmpgov.in/covid19";
 const sheetsURL = "http://127.0.0.1:5000/updateBulk";
+let result = [];
 
+// get the PowerBI Table link from the Covid Site
 axios({
   method: 'get',
   url: siteURL,
 }).then(res => {
-  // console.log(res.data);
-  let outputJsonArray = [];
   let $ = cheerio.load(res.data);
   let apiUrl = $("iframe").attr("src");
-  console.log(apiUrl);
-  callPowerBiApi(apiUrl)
+  translatePowerBiTable(apiUrl)
+      .then(result => {
+        update(result)
+            .then(response => {
+              console.log("Sheets API Update Response Status ", +response.status)
+            })});
 }).catch(error => {
   console.error(error)
 })
 
-async function callPowerBiApi(apiUrl) {
-  // const browser = await puppeteer.launch({headless: false, args: ['--auto-open-devtools-for-tabs']});
+async function translatePowerBiTable(apiUrl) {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  page.setDefaultNavigationTimeout(0);
-  await page.goto(apiUrl, {waitUntil: 'networkidle0', timeout: 0});
-  await page.waitForSelector('.innerContainer');
+  await page.setViewport({ width : 1200,height : 800})
+  await page.goto(apiUrl, {waitUntil: 'networkidle0'});
 
-  await page.evaluate(() => document.querySelectorAll(".innerContainer")[1].querySelector(".bodyCells>div div:last-of-type div:last-of-type div:last-of-type").scrollIntoView());
-  await page.evaluate(() => document.querySelectorAll(".innerContainer")[1].querySelector(".bodyCells>div div:last-of-type div:last-of-type div:last-of-type").scrollIntoView());
-  await page.evaluate(() => document.querySelectorAll(".innerContainer")[1].querySelector(".bodyCells>div div:last-of-type div:last-of-type div:last-of-type").scrollIntoView());
-  await page.evaluate(() => document.querySelectorAll(".innerContainer")[1].querySelector(".bodyCells>div div:last-of-type div:last-of-type div:last-of-type").scrollIntoView());
-  // await page.waitFor(2000);
-  await page.evaluate(() => document.querySelectorAll(".innerContainer")[1].querySelector(".bodyCells>div div:last-of-type div:last-of-type div:last-of-type").scrollIntoView());
-  await page.evaluate(() => document.querySelectorAll(".innerContainer")[1].querySelector(".bodyCells>div div:last-of-type div:last-of-type div:last-of-type").scrollIntoView());
-  await page.evaluate(() => document.querySelectorAll(".innerContainer")[1].querySelector(".bodyCells>div div:last-of-type div:last-of-type div:last-of-type").scrollIntoView());
-  await page.evaluate(() => document.querySelectorAll(".innerContainer")[1].querySelector(".bodyCells>div div:last-of-type div:last-of-type div:last-of-type").scrollIntoView());
-  await page.evaluate(() => document.querySelectorAll(".innerContainer")[1].querySelector(".bodyCells>div div:last-of-type div:last-of-type div:last-of-type").scrollIntoView());
-  await page.evaluate(() => document.querySelectorAll(".innerContainer")[1].querySelector(".bodyCells>div div:last-of-type div:last-of-type div:last-of-type").scrollIntoView());
-  let outputData = await page.evaluate(() => {
-    let leftData = document.querySelectorAll(".innerContainer")[1].querySelectorAll(".bodyCells>div div:first-of-type div");
-    // debugger;
-    leftData.push(document.querySelectorAll(".innerContainer")[1].querySelectorAll(".bodyCells>div div:nth-of-type(3) div"));
-    let rightData = document.querySelectorAll(".innerContainer")[1].querySelectorAll(".bodyCells>div div:nth-of-type(2) div");
-    rightData.push(document.querySelectorAll(".innerContainer")[1].querySelectorAll(".bodyCells>div div:nth-of-type(4) div"));
-    // console.log($(".innerContainer")[1]);
-    let cells = $(html).find(".bodyCells div:first-child");
-    console.log(cells);
+
+  /*
+    * BodyCells contain the data we need to scrape. It's the 3rd child of the innerContainer.
+    *   document.querySelectorAll(".innerContainer")[1].children[3].children[0]
+    * BodyCells are divided into two sides - left & right (unfortunately)
+    * Left side of BodyCells : from column Hospital Name to column Blocked General
+    *   document.querySelectorAll(".innerContainer")[1].children[3].children[0].children[0]
+    * Right side of BodyCells : from column Blocked HDU to column Net Avail. ICU Vent
+    *   document.querySelectorAll(".innerContainer")[1].children[3].children[0].children[1]
+    * Get a particular Column on either side
+    *   document.querySelectorAll(".innerContainer")[1].children[3].children[0].children[x].children[1]
+    * Get a row from a given column (cell) on either side
+    *   document.querySelectorAll(".innerContainer")[1].children[3].children[0].children[x].children[1].children[0]
+  */
+  for (let i = 0; i < 10; i++) {
+    let subResult = await page.evaluate(async() =>
+    {
+      //todo : something's up with scroll leading to a lot of hospital data not being recorded. Need's a fix
+      document.querySelectorAll(".innerContainer")[1]
+          .querySelector(".bodyCells div div:last-of-type div:last-of-type div:last-of-type")
+          .scrollIntoView();
+
+      // hospital count in current scroll view
+      let hospitalCount = document.querySelectorAll(".innerContainer")[1].children[3].children[0].children[0].children[0].children.length;
+      let localResult = [];
+      for (let j = 0; j < hospitalCount; j++) {
+        // populate hospital name ; 0th column on the left side
+        let hospitalName = document.querySelectorAll(".innerContainer")[1].children[3].children[0].children[0].children[0].children[j].innerText;
+        // populate general available ; 4th column on the right side
+        let genAvail = document.querySelectorAll(".innerContainer")[1].children[3].children[0].children[1].children[3].children[j].innerText;
+        // populate HDU available ; 5th column on the right side
+        let hduAvail = document.querySelectorAll(".innerContainer")[1].children[3].children[0].children[1].children[4].children[j].innerText;
+        // populate ICU available ; 6th column on the right side
+        let icuAvail = document.querySelectorAll(".innerContainer")[1].children[3].children[0].children[1].children[5].children[j].innerText;
+        // populate ICU Vent available ; 7th column on the right side
+        let icuVentAvail = document.querySelectorAll(".innerContainer")[1].children[3].children[0].children[1].children[6].children[j].innerText;
+
+        localResult.push({
+          "Name":hospitalName,
+          "COVID Beds":genAvail,
+          "HDU Beds":hduAvail,
+          "ICU":icuAvail,
+          "Ventilator Beds":icuVentAvail
+        });
+      }
+      return localResult;
+    });
+    result.push.apply(result, subResult);
+  }
+  return result;
+}
+
+// Update Hospital Data to Google Sheets
+async function update(hospitalData) {
+  // remove duplicates that are introduced by the scrolling issues
+  let uniqueHospitalNamesList = []
+  let uniqueHospitals = [];
+  for (let i = 0; i< hospitalData.length; i++){
+    if (!uniqueHospitalNamesList.includes(hospitalData[i].Name)){
+      uniqueHospitalNamesList.push(hospitalData[i].Name);
+      uniqueHospitals.push(hospitalData[i]);
+    }
+  }
+  // Uncomment to validate the hospital data sent to sheets api
+  // console.log(uniqueHospitals);
+  // console.log("Number of unique hospitals", +uniqueHospitals.length);
+
+  // make a call to sheets api
+  // todo : we can move this code to a common module to avoid boiler plate code in multiple files
+  return await fetch(sheetsURL, {
+    method: 'POST', // or 'PUT'
+    credentials: 'omit',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(uniqueHospitals),
   })
 }
