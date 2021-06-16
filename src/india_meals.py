4a8a6a2c41a9d92f49b11ec27bfd67f61a6849a6
ampyoso <apoorvamn1990@gmail.com>
2021-06-16 02:52:19 -0700

    Further improved success rate, super slow :(

--- src/india_meals.py
+++ src/india_meals.py
@@ -6,9 +6,11 @@ from bs4 import BeautifulSoup as bs
 from collections import OrderedDict
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException
 import time
 import sys
 if sys.version_info[0] >= 3:
@@ -30,6 +32,7 @@ if __name__ == '__main__':
         openDropdown()
         soup = bs(browser.page_source, 'html.parser')
         actions = ActionChains(browser)
+        count = 0
 
         for ultag in soup.find_all('ul', {'class': 'MuiList-root MuiMenu-list MuiList-padding'}):
             for index, litag in enumerate(ultag.find_all('li')):
@@ -39,13 +42,40 @@ if __name__ == '__main__':
                 if(index != 1):
                     openDropdown()
                     
-                # elem = browser.switch_to.active_element
-                # webElement = browser.find_element(By.XPATH, "/html/body/div[2]/div[3]/ul/li[{}]".format(index))
-                # actions.move_to_element(webElement).click().perform()
-                WebDriverWait(browser,1).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[3]/ul/li[{}]".format(index)))).click()
-                WebDriverWait(browser,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div[2]/div/button/span[1]/button/span[1]"))).click()
-                time.sleep(2)
+                try:
+                    # elem = browser.switch_to.active_element
+                    webElement = browser.find_element(By.XPATH, "/html/body/div[2]/div[3]/ul/li[{}]".format(index+1))
+                    time.sleep(1)
+                    actions = ActionChains(browser)
+                    actions.move_to_element(webElement).click(on_element=webElement).perform()
+                    # WebDriverWait(browser,2).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/ul/li[{}]".format(index+1)))).click()
+                except ElementClickInterceptedException:
+                    print("Err 1 ", litag.text)
+                    time.sleep(10)
+                
+                try:    
+                    WebDriverWait(browser,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div[2]/div/button/span[1]/button/span[1]"))).click()
+                    # html = browser.find_element_by_tag_name('html')
+                    # html.send_keys(Keys.END)
+                    time.sleep(10)
+                except ElementClickInterceptedException:
+                    print("Err 2 ", litag.text)
+                    time.sleep(10)
+
+                last_height = browser.execute_script("return document.body.scrollHeight")
+                while True:
+                    # Scroll down to bottom
+                    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                    # Wait to load page
+                    time.sleep(2)
+                    # Calculate new scroll height and compare with last scroll height
+                    new_height = browser.execute_script("return document.body.scrollHeight")
+                    if new_height == last_height:
+                        break
+                    last_height = new_height
+                
                 results = browser.find_elements_by_xpath("/html/body/div/div/div/div[4]/div[2]/button")
+                # print(litag.text, len(results))
                 for result in results:
                     data = OrderedDict()
                     elementHTML = result.get_attribute('outerHTML')
@@ -57,21 +87,26 @@ if __name__ == '__main__':
                     try:
                         serviceType = soup.find("div", {"class" : "Style_service_type__3JHoA"}).find('p').text.strip()
                     except:
-                        "N/A"
+                        serviceType = "N/A"
                     try:    
                         pickup = soup.find("div", {"class" : "Style_pickup__195UR"}).find('p').text.strip()
                     except:
-                        "N/A"
+                        pickup = "N/A"
                     try:
                         hours = soup.find("div", {"class" : "Style_time__Eefwi"}).text.encode("ascii", "ignore").decode()
                     except:
-                        "N/A"
+                        hours = "N/A"
                     data['Comments'] = "Service type: " + serviceType + ", Pickup: " + pickup + ", Hours: " + hours
-                    api_input.append(data)    
+                    count += 1
+                    api_input.append(data)
+                    
+                html = browser.find_element_by_tag_name('html')
+                html.send_keys(Keys.UP)        
 
         browser.close()
+        print("All done ", count)
         data = json.dumps(api_input)
-        print(data)
+        # print(data)
         
         # api_response = requests.post(api_url, json=json.loads(data), verify=False)
         # if api_response.status_code != 200:
